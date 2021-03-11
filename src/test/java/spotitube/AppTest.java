a134ee4557459f91f3a865f17ff0d7508dd3b8b1
nehlis <nielsbosman@mail.com>
2021-03-11 11:35:08 +0100

    Fix pom.xml file

--- src/test/java/spotitube/AppTest.java
+++ src/test/java/spotitube/AppTest.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
