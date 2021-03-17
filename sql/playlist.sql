2ad33c34d11642ad8edf8516adcc7b5f36544984
nehlis <nielsbosman@mail.com>
2021-03-17 13:21:34 +0100

    Base SQL files

--- sql/playlist.sql
+++ sql/playlist.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS playlist;
 CREATE TABLE playlist
 (
     id INT NOT NULL auto_increment PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    user_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO playlist (name) VALUES ('afspeellijst');
+INSERT INTO playlist (name, user_id) VALUES ('afspeellijst', 0);
--- sql/playlist_user.sql
+++ sql/playlist_user.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS playlist_user;
 CREATE TABLE playlist_user
 (
     id INT NOT NULL auto_increment PRIMARY KEY,
-    user_id int NOT NULL,
-    playlist_id int NOT NULL,
+    user_id INT NOT NULL,
+    playlist_id INT NOT NULL
 );
 
 INSERT INTO playlist_user (user_id, playlist_id) VALUES (0, 0);
